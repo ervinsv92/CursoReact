692905e8e2d7b82eecd4b19bdab79cd293bbaa97
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-06-18 11:53:39 -0600

    Hasta clase 45

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,33 +1,38 @@
 //import React, {Fragment} from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 //const PrimeraApp = ({saludo ='Hola mundo'}) =>{
-const CounterApp = ({value}) =>{
+const CounterApp = ({ value = 10 }) => {
 
-    const handledAdd = (e) =>{
+    const [counter, setCounter] = useState(value);
 
+    const handledAdd = (e) => {
+        setCounter(counter + 1);
+        //setCounter((c) => c + 1); // casi no se usa
     }
 
-    return (
+    const handledReset = (e) => setCounter(value);
+    const handledSubstract = (e) => {
+        setCounter(counter - 1);
+    }
+
+    return ( 
         <>
-            <h1>Counter App</h1>
-            <h2>{value}</h2>
-            {/* Al agregar la funcion al boton no se pone () porque se ocupa que se ejecute despues de que se renderiza */}
-            <button onClick={handledAdd}>+1</button>
+            <h1> Counter App </h1>  
+            <h2> { counter } </h2>   
+            <button onClick = { handledAdd } > +1 </button> 
+            <button onClick = { handledReset } > Reset </button> 
+            <button onClick = { handledSubstract } > -1 </button> 
         </>
     );
 }
 
 /*
     Se indica el formato de los props y si son requeridos
-*/ 
+*/
 CounterApp.propTypes = {
-    value:PropTypes.number
-}
-
-CounterApp.defaultProps={
-    subtitulo:'Soy un subtitulo desde las props'
+    value: PropTypes.number
 }
 
 export default CounterApp;
