da0bcc9bb75cf18b5c3373e93a620c9ea7e222c3
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-06-28 13:31:29 -0600

    Hasta clase 118

---
+++ 05-hook-app/src/components/06-memos/CallbackHook.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import { ShowIncrement } from './ShowIncrement';
+import '../02-useEffect/effects.css';
+
+export const CallbackHook = ()=> {
+
+    const [counter, setCounter] = useState(10);
+    /*
+    const increment = () => {
+        setCounter(counter +1);
+    }*/
+
+    /**
+     * useCallback sirve para que el componente no se monte cada vez que se renderiza la pantalla, si tiene una funcion que se ejecuta cuando es creado
+     * Tambien depende del React.memo
+     * Se usa cuando se envia una funcion por property
+     */
+    const increment = useCallback(
+        (num) => {
+            setCounter(c => c +num);
+        },
+        [setCounter],
+    )
+
+    useEffect(() => {
+        //???
+    }, [increment])
+
+    return (
+        <div>
+            <h1>useCallback Hook: {counter}</h1>
+            <hr/>
+
+            <ShowIncrement increment={increment}/>
+        </div>
+    )
+}
