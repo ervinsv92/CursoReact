a84c776c19a7029c2b8bb8622201a4e6153ac19a
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-06-28 09:36:49 -0600

    Hasta clase 116

---
+++ 05-hook-app/src/components/06-memos/Small.js
@@ -0,0 +1,12 @@
+import React from 'react'
+/**
+ * Memo se usa para que le componente no se renderize si sus datos no han cambiado, evitando asi posibles llamados http inecesarios con datos iguales.
+ * Solo se dispara si las propertys cambian
+ */
+export const Small = memo(({value}) => {
+    return (
+        <small>
+            {value}
+        </small>
+    )
+})
