018232b26b725e9ee589bb9a47e2b63175328e2c
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-06-28 10:34:02 -0600

    Hasta clase 117

--- 05-hook-app/src/components/06-memos/MemoHook.js
+++ 05-hook-app/src/components/06-memos/MemoHook.js
@@ -1,17 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
+import { procesoPesado } from '../../helpers/procesoPesado';
 import { useCounter } from '../../hooks/useCounter';
-import {Small} from './Small';
 import '../02-useEffect/effects.css';
 
 export const MemoHook = () => {
 
-    const {counter, increment} = useCounter(10);
+    const {counter, increment} = useCounter(5000);
     const [show, setShow] = useState(true);
+
+    const memoProcesoPesado = useMemo(() => procesoPesado(counter), [counter]);
 
     return (
         <div>
-            <h1>Counter: <Small value={counter} /> </h1>
+            <h1>Counter: <small>{counter}</small> </h1>
             <hr/>
+
+            <p>{memoProcesoPesado}</p>
+
             <button 
                 className="btn btn-primary"
                 onClick={increment}
--- 05-hook-app/src/components/06-memos/Small.js
+++ 05-hook-app/src/components/06-memos/Small.js
@@ -3,7 +3,7 @@ import React from 'react'
  * Memo se usa para que le componente no se renderize si sus datos no han cambiado, evitando asi posibles llamados http inecesarios con datos iguales.
  * Solo se dispara si las propertys cambian
  */
-export const Small = memo(({value}) => {
+export const Small = React.memo(({value}) => {
     return (
         <small>
             {value}
