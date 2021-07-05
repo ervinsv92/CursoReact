70d0de2c38e8081878d1d7a3c06f9bd78f825adf
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-07-05 17:52:26 -0600

    Hasta clase 132

--- 05-hook-app/src/components/08-useReducer/TodoApp.js
+++ 05-hook-app/src/components/08-useReducer/TodoApp.js
@@ -1,21 +1,17 @@
 import React, {useReducer, useEffect, useCallback} from 'react'
 import './styles.css';
 import { todoReducer } from './todoReducer';
-import {useForm} from '../../hooks/useForm';
+
 import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
 
 const init = ()=>{
     return JSON.parse(localStorage.getItem('todos')) || [];
 }
 
 export const TodoApp = () => {
-
     //const [todos], se hace con destructuracion de arreglos, por lo tanto todo puede ser cualquier nombre
     const [todos, dispatch] = useReducer(todoReducer, [], init);
-
-    const [{description}, handleInputChange, reset] = useForm({
-        description:''
-    });
 
     //Se ejecuta cuando cambia el valor de un estado
     useEffect(() => {
@@ -42,28 +38,6 @@ export const TodoApp = () => {
         });
     }
 */
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        if(description.trim().length <= 1){
-            return;
-        }
-
-        const newTodo = {
-            id: new Date().getTime(),
-            desc:description,
-            done:false
-        };
-
-        const action = {
-            type: 'add',
-            payload: newTodo
-        };
-
-        //Manda la accion al reducer
-        dispatch(action);
-        reset();
-    }
 
     //para el ejemplo no es necesario utilizar el callback, con solo pasar una funcion normal sirve
     const handleToggle = useCallback(
@@ -87,6 +61,14 @@ export const TodoApp = () => {
         [dispatch],
     )
 
+    const handleAddTodo = (newTodo) =>{
+        //Manda la accion al reducer
+        dispatch({
+            type: 'add',
+            payload: newTodo
+        });
+    }
+
     return (
         <div>
             <h1>TodoApp ({todos.length})</h1>
@@ -97,15 +79,7 @@ export const TodoApp = () => {
                 <TodoList todos={todos} handleDelete={handleDelete} handleToggle={handleToggle}></TodoList>
                 </div>
                 <div className="col-5">
-                    <h4>Agregar TODO</h4>
-                    <hr></hr>
-                    <form onSubmit={handleSubmit}>
-                        <input type="text" name="description" placeholder="Aprender ..." autoComplete="off" className="form-control" 
-                        onChange={handleInputChange}
-                        value={description}
-                    />
-                        <button className="btn btn-outline-primary mt-1 btn-block" type="submit">Agregar</button>
-                    </form>
+                    <TodoAdd handleAddTodo={handleAddTodo}></TodoAdd>
                 </div>
             </div>
         </div>
