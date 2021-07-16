e6e1df1fb5bf4374fb5a4b36b08073f41998c784
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-07-15 18:01:03 -0600

    Hasta clase 183

--- 07-heroes-app/src/router/DashboardRoutes.js
+++ 07-heroes-app/src/router/DashboardRoutes.js
@@ -3,6 +3,7 @@ import { Redirect, Route, Switch } from 'react-router-dom'
 import { DcScreen } from '../components/dc/DcScreen'
 import { HeroScreen } from '../components/heroes/HeroScreen'
 import { MarvelScreen } from '../components/marvel/MarvelScreen'
+import { SearchScreen } from '../components/search/SearchScreen'
 import { Navbar } from '../components/ui/Navbar'
 /**
  *  Es un sistema de rutas secundario, la unica diferencia con el principal es que no se le define el router, solo se usa el switch
@@ -17,6 +18,7 @@ export const DashboardRoutes = () => {
                     <Route exact path='/marvel' component={MarvelScreen}></Route>
                     <Route exact path='/hero/:heroId' component={HeroScreen}></Route>
                     <Route exact path='/dc' component={DcScreen}></Route>
+                    <Route exact path='/search' component={SearchScreen}></Route>
                     <Redirect to="/marvel"></Redirect>
                 </Switch>
             </div>
