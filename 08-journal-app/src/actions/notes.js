7817268a01e37642cb63477d1c3b1651d9e4fa4f
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-07-28 13:49:09 -0600

    Hasta clase 267

--- 08-journal-app/src/actions/notes.js
+++ 08-journal-app/src/actions/notes.js
@@ -36,3 +36,23 @@ export const setNotes = (notes)=>({
     type: types.notesLoad,
     payload: notes  
 })
+
+export const startSaveNote = (note)=>{
+    return async (dispatch, getState) =>{
+        const {uid} = getState().auth;
+        /*
+        if(!note.url){
+            delete note.url;
+        }*/
+
+        const noteToFirestore = {...note};
+        //Elimina el atributo id del objeto
+        delete noteToFirestore.id;
+        
+        if(!noteToFirestore.url){
+            delete noteToFirestore.url;
+        }
+
+        await db.doc(`${uid}/journal/notes/${note.id}`).update(noteToFirestore);
+    }
+}
--- 08-journal-app/src/components/notes/NoteScreen.js
+++ 08-journal-app/src/components/notes/NoteScreen.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useRef } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { activeNote } from '../../actions/notes'
 import { useForm } from '../../hooks/useForm'
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
 
+    //Sirve para poder ejecutar acciones
+    const dispatch = useDispatch();
     const {active:note} = useSelector(state => state.notes)
     const [formValues, handleInputChange, reset] = useForm(note);
     const {body, title} = formValues;
@@ -22,6 +25,12 @@ export const NoteScreen = () => {
        }
     }, [note, reset])
 
+    //Se ejecuta cuando algo cambia, en este caso el formValues
+    useEffect(() => {
+        //actualiza el estado en redux cuando el formulario cambia
+        dispatch(activeNote(formValues.id, {...formValues}));
+    }, [formValues, dispatch])
+
     return (
         <div className="notes__main-content">
             <NotesAppBar />
@@ -32,6 +41,7 @@ export const NoteScreen = () => {
                     placeholder="Some awesome title" 
                     className="notes__title-input" 
                     autoComplete="off"
+                    name="title"
                     value={title}
                     onChange={handleInputChange}
                 />
@@ -39,6 +49,7 @@ export const NoteScreen = () => {
                 <textarea 
                     placeholder="What happened today" 
                     className="notes__textarea"
+                    name="body"
                     value={body}
                     onChange={handleInputChange}
                 ></textarea>
