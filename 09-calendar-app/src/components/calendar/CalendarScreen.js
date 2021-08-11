371ea6899207c6b3ef71a61205140b89cf79db5a
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-08-11 17:32:33 -0600

    Hasta clase 307

--- 09-calendar-app/src/components/calendar/CalendarScreen.js
+++ 09-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,10 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Calendar, momentLocalizer} from 'react-big-calendar';
+import moment from 'moment';
+import 'moment/locale/es';
 import { Navbar } from '../ui/Navbar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { messages } from '../../helpers/calendar-messages-es';
+import { CalendarEvent } from './CalendarEvent';
+moment.locale('es');
+const localizer = momentLocalizer(moment) 
+
+const events = [{
+    title:'Cumpleaños',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa',
+    notes:'Comprar el pastel',
+    user:{
+        _id:'123',
+        name:'Ervin'
+    }
+}];
 
 export const CalendarScreen = () => {
+
+    const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month');
+
+    const onDoubleClick = (e)=>{
+
+    }
+
+    const onSelectEvent = (e)=>{
+
+    }
+
+    const onViewChange = (e)=>{
+        setLastView(e);
+        localStorage.setItem('lastView', e)
+    }
+
+
+    const eventStyleGetter = (event, start, end, isSelected)=>{
+        const style = {
+            backgroundColor:'#367CF7',
+            borderRadius: '0px',
+            opacity:0.8,
+            display:'block',
+            color: 'white'
+        }
+        return {
+            style
+        }
+    }
+
     return (
-        <div>
+        <div className="calendar-screen">
             <Navbar />
+
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                messages={messages}
+                eventPropGetter={eventStyleGetter}
+                onDoubleClickEvent={onDoubleClick}
+                onSelectEvent={onSelectEvent}
+                onView={onViewChange}
+                view={lastView}
+                components={{
+                    event:CalendarEvent
+                }}
+                />
         </div>
     )
 }
