59519775d75c2b4db511c36508d7095a57f93091
ervinsv92 <ervinsv92@gmail.com>
2021-08-29 18:16:32 -0600

    Hasta clase 324

---
+++ 10-calendar-backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.json({
+        ok:true
+    })
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
+});
