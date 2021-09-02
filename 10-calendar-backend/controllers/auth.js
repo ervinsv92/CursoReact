90389344b0e644b9ed0f7b1dc5093d0bb2293095
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-09-02 17:11:39 -0600

    Hasta clase 327

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -2,20 +2,39 @@ const {response} = require('express');
 
 //express response es para que el intelicense ayude a autocompletar
 const crearUsuario = (req, res = response) =>{
+
+    const {name, email, password} = req.body;
+
+    //Asegurarse de que solo se envie una respuesta dentro de una peticion, para eso se puede usar el return
+    if(name.length < 5){
+        return res.status(400).json({
+            ok:false,
+            msg:"El nombre debe ser de 5 letras"
+        })
+    }
+
     res.json({
         ok:true,
-        msg:'registro'
+        msg:'registro',
+        name, 
+        email, 
+        password
     })
 }
 
 const loginUsuario = (req, res = response) =>{
+
+    const {email, password} = req.body;
+
     res.json({
         ok:true,
-        msg:'login'
+        msg:'login',
+        email, 
+        password
     })
 }
 
-const revalidarToken = (req, res) =>{
+const revalidarToken = (req, res = response) =>{
     res.json({
         ok:true,
         msg:'renew'
