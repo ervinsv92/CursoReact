e01a2456815a2c8cf9274da2f875aefb256c7845
ervinsv92 <ervinsv92@gmail.com>
2021-09-05 12:36:01 -0600

    Hasta clase 341

--- 10-calendar-backend/routes/auth.js
+++ 10-calendar-backend/routes/auth.js
@@ -8,6 +8,7 @@ const router = Router();
 const {check} = require('express-validator');
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
+const {validarJWT} = require('../middlewares/validar-jwt');
 
 router.post(
     '/new', 
@@ -24,6 +25,7 @@ router.post('/',
     check('password', 'El password debe ser de 6 caracteres').isLength({min:6}),
     validarCampos
 ], loginUsuario);
-router.get('/renew', revalidarToken);
+
+router.get('/renew', validarJWT, revalidarToken);
 
 module.exports = router;
