b2c266ea337e25d94d36e1f0e1899fed99feec1e
ervinsv92 <ervinsv92@gmail.com>
2021-09-04 18:29:01 -0600

    Hasta clase 335

--- 10-calendar-backend/controllers/auth.js
+++ 10-calendar-backend/controllers/auth.js
@@ -1,9 +1,10 @@
 const {response} = require('express');
+const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
 
 //express response es para que el intelicense ayude a autocompletar
 const crearUsuario = async (req, res = response) =>{
-    //const {name, email, password} = req.body;
+    const {email, password} = req.body;
 
     //Asegurarse de que solo se envie una respuesta dentro de una peticion, para eso se puede usar el return
     /*
@@ -14,15 +15,31 @@ const crearUsuario = async (req, res = response) =>{
         })
     }*/
     try {
-        const usuario = new Usuario(req.body);
+        let usuario = await Usuario.findOne({
+            email
+        });
+
+        if(usuario){
+            return res.status(400).json({
+                ok:false,
+                msg:'Un usuario existe con ese correo.'
+            })
+        }
+
+        usuario = new Usuario(req.body);
+
+        const salt = await bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
 
         await usuario.save();
 
         res.status(201).json({
             ok:true,
-            msg:'registro'
+            uid: usuario.id,
+            name:usuario.name
         });    
     } catch (error) {
+        console.log(error)
         res.status(500).json({
             ok:false,
             msg:'Por favor hable con el administrador'
@@ -31,15 +48,42 @@ const crearUsuario = async (req, res = response) =>{
     
 }
 
-const loginUsuario = (req, res = response) =>{
+const loginUsuario = async (req, res = response) =>{
     const {email, password} = req.body;
 
-    res.status(200).json({
-        ok:true,
-        msg:'login',
-        email, 
-        password
-    })
+    try {
+        const usuario = await Usuario.findOne({
+            email
+        });
+
+        if(!usuario){
+            return res.status(400).json({
+                ok:false,
+                msg:'El usuario no existe con ese email.'
+            })
+        }
+
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+
+        if(!validPassword){
+            return res.status(400).json({
+                ok:false,
+                msg:'Password incorrecto'
+            });
+        }
+
+        res.status(400).json({
+            ok:false,
+            uid:usuario.id,
+            name: usuario.name
+        });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok:false,
+            msg:'Por favor hable con el administrador'
+        })
+    }
 }
 
 const revalidarToken = (req, res = response) =>{
