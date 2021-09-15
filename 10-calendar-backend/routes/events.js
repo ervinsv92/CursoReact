040c5e7eb1345b99fa0c287e9d9fb7fd532184b3
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-09-15 15:38:37 -0600

    Hasta clase 345

--- 10-calendar-backend/routes/events.js
+++ 10-calendar-backend/routes/events.js
@@ -2,11 +2,19 @@ const { Router } = require('express');
 const {validarJWT} = require('../middlewares/validar-jwt');
 const router = Router();
 router.use(validarJWT);
-//const {check} = require('express-validator');
+const {check} = require('express-validator');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
-//const { validarCampos } = require('../middlewares/validar-campos');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { isDate } = require('../helpers/isDate');
 
-router.get('/', getEventos);
+router.get('/', 
+        [
+            check('title', 'El título es obligatorio').not().isEmpty(),
+            check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+            check('end', 'Fecha de fin es obligatoria').custom(isDate),
+            validarCampos
+        ],
+        getEventos);
 router.post('/', crearEvento);
 router.put('/:id', actualizarEvento);
 router.delete('/:id', eliminarEvento);
