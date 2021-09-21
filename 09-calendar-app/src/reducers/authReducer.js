e3b8bafb26edaf8b4133a5192f909284b6f1587d
Ervin Solano Vargas <esolanov@grupocolono.com>
2021-09-21 16:15:57 -0600

    Hasta clase 370

--- 09-calendar-app/src/reducers/authReducer.js
+++ 09-calendar-app/src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import { types } from "../types/types";
+
 const initialState = {
     checking:true,
     //uid:null,
@@ -7,6 +9,21 @@ const initialState = {
 export const authReducer = (state = initialState, action) =>{
     switch (action.type) {
     
+        case types.authLogin:
+            return {
+                ...state,
+                checking:false,
+                ...action.payload
+            }
+        case types.authCheckingFinish:
+            return {
+                ...state,
+                checking:false
+            }
+        case types.authLogout:
+            return {
+                checking:false
+            }
         default:
             return state;
     }
